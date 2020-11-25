d4d2d2d5e54e629bf54a1c09c892bef67103fd6f
Romel Palomino <romel.palomino@rappi.com>
2020-11-25 05:15:56 -0500

    Implementar la anotacion repository

--- src/main/java/com/romelapj/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/romelapj/platzimarket/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.romelapj.platzimarket.persistence;
 
 import com.romelapj.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.romelapj.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -20,5 +22,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
